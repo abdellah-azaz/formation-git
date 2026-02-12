b5d4d4a486d943ab42d93ebc45ba280918f5bf00
abdellah azaz <abdellahazaz5@gmail.com>
2026-02-12 22:46:12 +0100

    first commit

---
+++ src/pages/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <ul>
+        <li>Web Development</li>
+        <li>UI/UX Design</li>
+        <li>Mobile Apps</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Services;
